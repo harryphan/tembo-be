48413fa4f8dccb15123c26b045f6d0f33e23e184
Harry Phan <harryphan@gmail.com>
2023-01-12 20:52:01 -0500

    user model.

---
+++ src/models/User.ts
@@ -0,0 +1,41 @@
+import { DataTypes, Model, Optional } from 'sequelize'
+import sequelizeConnection from '../db-config';
+
+interface UserAttributes {
+    id: number;
+    username: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+}
+
+export interface UserInput extends Optional<UserAttributes, 'id'> {}
+
+export interface UserOutput extends Required<UserAttributes> {}
+
+class User extends Model<UserAttributes, UserInput> implements UserAttributes {
+    public id!: number
+    public username!: string
+
+    // timestamps!
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+    public readonly deletedAt!: Date;
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize: sequelizeConnection,
+    modelName: 'Users'
+})
+
+export default User;
